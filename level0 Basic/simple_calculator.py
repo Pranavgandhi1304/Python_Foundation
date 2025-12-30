3e2a153446651b744031f82b88e95a66262b1f5b
Pranavgandhi1304 <pranav13.04.06@gmail.com>
2025-12-30 15:10:32 +0530

    Add files via upload

---
+++ level0 Basic/simple_calculator.py
@@ -0,0 +1,13 @@
+# converting string input to float
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+# doing all operations
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+print(f"Addition: {addition}")
+print(f"Subtraction: {subtraction}")
+print(f"Multiplication: {multiplication}")
+print(f"Division: {division}")
